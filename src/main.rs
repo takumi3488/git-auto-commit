97f6a4d55d1f569910ba80ddb9e7eedac78b0999
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-07-22 22:47:06 +0900

    'Updated at: 2022/07/22 22:47:06'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use chrono::Local;
+use clap::Parser;
+use std::process::Command;
+
+#[derive(Parser)]
+#[clap(version)]
+struct Cli {
+    #[clap(short, long)]
+    push: bool,
+}
 fn main() {
-    println!("Hello, world!");
+    Command::new("git").args(&["add", "-A"]).output().unwrap();
+    Command::new("git")
+        .args(&[
+            "commit",
+            "-m",
+            &Local::now()
+                .format("'Updated at: %Y/%m/%d %H:%M:%S'")
+                .to_string(),
+        ])
+        .output()
+        .unwrap();
+    if Cli::parse().push {
+        Command::new("git")
+            .args(&["push", "origin", "main"])
+            .output()
+            .unwrap();
+    }
 }
